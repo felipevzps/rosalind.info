2918e8c670cb600c31d8d2c60332ecd1950c828a
felipevzps <felipe.vzps@gmail.com>
2020-11-26 20:00:49 -0300

    Added #13 RNA Splicing

---
+++ Bioinformatics Stronghold/#13 RNA Splicing.py
@@ -0,0 +1,31 @@
+from Bio.Seq import Seq
+from Bio import SeqIO
+
+introns = []
+sequences_in_dna_file = 0
+seq_count = 0
+
+# Getting number of sequences in dna file
+with open("rosalind_splc.txt", "r") as dna_file:
+    for line in dna_file.readlines():
+        if line.startswith(">"):
+            sequences_in_dna_file += 1
+
+# Getting DNA strand and introns
+with open("rosalind_splc.txt", "r") as dna_file:
+    records = list(SeqIO.parse(dna_file, "fasta"))
+    # first string (0) is the DNA strand
+    for i in range(1,sequences_in_dna_file):
+        seq_count += 1
+        dna = str(records[0].seq)
+        introns.append(str(records[seq_count].seq))
+
+# Removing introns from DNA strand
+for intron in introns:
+    if intron in dna:
+       dna = dna.replace(intron, "")
+
+# Translate DNA strand (DNA --> RNA --> PROTEIN)
+seq = Seq(dna)
+protein = seq.translate(stop_symbol="")
+print("protein = ", protein)
