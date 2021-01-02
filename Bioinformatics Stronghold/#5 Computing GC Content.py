89334ad8855a6211cad3c1601f82868549e4a3db
Felipe Vaz Peres <felipe.vzps@gmail.com>
2021-01-01 23:53:40 -0300

    Update #5 Computing GC Content.py

--- Bioinformatics Stronghold/#5 Computing GC Content.py
+++ Bioinformatics Stronghold/#5 Computing GC Content.py
@@ -5,9 +5,9 @@ sequences = {}
 for record in SeqIO.parse("Bioinformatics Stronghold/teste.txt", "fasta"):
     sequences[record.id] = GC(record.seq)
 
-maximo = max(sequences.values())
-get_key = [key for (key, value) in sequences.items() if value == maximo]
+maxValue = max(sequences.values())
+get_key = [key for (key, value) in sequences.items() if value == maxValue]
 
 # I am using * to remove the brackets from the list 
 # and "sep" to remove spaces of the start/end in the print
-print(*get_key,"\n",maximo, sep="")
+print(*get_key, "\n", maxValue, sep="")
