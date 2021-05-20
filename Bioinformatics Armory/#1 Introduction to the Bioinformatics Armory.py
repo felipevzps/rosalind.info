2d9c6d1266ab9f93af9fa4a038ed21105e6feaf8
felipevzps <felipe.vzps@gmail.com>
2021-05-20 12:58:40 -0300

    updating data

--- Bioinformatics Armory/#1 Introduction to the Bioinformatics Armory.py
+++ Bioinformatics Armory/#1 Introduction to the Bioinformatics Armory.py
@@ -1,8 +1,8 @@
 from Bio.Seq import Seq
 
-with open("rosalind_ini.txt", "r") as sequence_file:
+with open("data/rosalind_ini.txt", "r") as sequence_file:
     sequence = Seq(sequence_file.read())
 
-# Using count_overlap bcause python string's .count() is a non-overlapping count
-# In some biological situations, overlapping count is necessary
+#Using count_overlap bcause python string's .count() is a non-overlapping count
+#In some biological situations, overlapping count is necessary
 print(sequence.count_overlap("A"), sequence.count_overlap("C"), sequence.count_overlap("G"), sequence.count_overlap("T"))
--- Bioinformatics Armory/#2 Introduction to Protein Databases.py
+++ Bioinformatics Armory/#2 Introduction to Protein Databases.py
@@ -2,15 +2,16 @@ from Bio import ExPASy
 from Bio import SwissProt
 import itertools 
 
-with open("rosalind_dbpr.txt", "r") as sample:
+with open("data/rosalind_dbpr.txt", "r") as sample:
     UniProt_ID = sample.read()
     UniProt_ID = UniProt_ID.rstrip()
 
 handle = ExPASy.get_sprot_raw(UniProt_ID)
 record = SwissProt.read(handle)
-print(record.cross_references)
+#print(record.cross_references)
+
 for i in record.cross_references:
-    # Getting biological process (P) in the Gene Ontology (GO) tuple
+    #Getting biological process (P) in the Gene Ontology (GO) tuple
     if "GO" in i and "P:" in i[2]:
         print(i[2].replace('P:', ''))
     
