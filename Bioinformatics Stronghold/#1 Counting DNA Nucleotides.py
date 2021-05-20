54035488a28b78bb4c82b1cf9f0f22dc8cbc3e90
felipevzps <felipe.vzps@gmail.com>
2021-05-20 12:21:54 -0300

    add data repo

--- Bioinformatics Stronghold/#1 Counting DNA Nucleotides.py
+++ Bioinformatics Stronghold/#1 Counting DNA Nucleotides.py
@@ -1,6 +1,7 @@
-file = open('/home/felipe/Documentos/vscode/Rosalind/rosalind_dna.txt', 'r')
+file = open('data/rosalind_dna.txt', 'r')
 DNAstring = file.read()
 
+#using a function
 def counter(n):
     baseSum = 0
     for i in DNAstring:
@@ -8,4 +9,7 @@ def counter(n):
             baseSum += 1
     return baseSum
 
-print(counter('a'), counter('c'), counter('g'), counter('t'))
+print(counter('A'), counter('C'), counter('G'), counter('T'))
+
+#pythonic way
+print(DNAstring.coun('A'),DNAstring.coun('C'),DNAstring.coun('G'),DNAstring.coun('T'))
